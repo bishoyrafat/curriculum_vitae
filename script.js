a152aa955f9917b4c26e7104c1393618d3d05281
bishoyrafat <bishoyrafat@yahoo.com>
2026-03-22 15:01:41 +0100

    enhance summary section

--- script.js
+++ script.js
@@ -3,10 +3,11 @@
 const summaryList = document.querySelector(".summary__list");
 
 const summary = [
-  "Front-End Developer with 3+ years of professional Angular experience, building scalable web applications. Experienced in modern UI develop- ment, component-based architecture, and Agile teamwork.",
-  "Currently pursuing a Master's degree in Informatics in Prague and seeking a Front-End Developer role.",
-,"Ready to join immediately"
+
+  "Front-End Developer with 3+ years of experience specializing in Angular, delivering scalable andhigh-performance web applications. Strong background in modern UI development, componentbased architecture, and collaborating in Agile environments",
+"Currently pursuing a Master’s degree in Informatics in Prague and seeking a Front-End Developer rolewhere I can contribute to building efficient, user-focused interfaces while continuing to growtechnically. Available to start immediately"
 ];
+
 
 summary.forEach((item) => {
   const listItem = document.createElement("li");
